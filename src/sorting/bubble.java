212fedb4bbdddb1302e1e63321e1563d13451402
Jayanth <Jayanth@DESKTOP-OCE30GK>
2021-02-03 15:56:58 +0530

    merged

--- src/sorting/bubble.java
+++ src/sorting/bubble.java
@@ -8,14 +8,14 @@ public class bubble {
             for (int j = 0; j < n-i-1; j++)
                 if (arr[j] > arr[j+1])
                 {
-                    // swap temp and arr[i]
+                   
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
     }
  
-    /* Prints the array */
+   
     void printArray(int arr[])
     {
         int n = arr.length;
